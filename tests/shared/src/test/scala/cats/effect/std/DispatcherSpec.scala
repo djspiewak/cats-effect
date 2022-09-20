ac910ed2939cc4eff32a90d14c5434d7884955fc
Daniel Spiewak <djspiewak@gmail.com>
2022-09-20 08:52:07 +0200

    WIP

--- tests/shared/src/test/scala/cats/effect/std/DispatcherSpec.scala
+++ tests/shared/src/test/scala/cats/effect/std/DispatcherSpec.scala
@@ -27,7 +27,7 @@ class DispatcherSpec extends BaseSpec {
 
   override def executionTimeout = 30.seconds
 
-  "sequential dispatcher" should {
+  /*"sequential dispatcher" should {
     "await = true" >> {
       val D = Dispatcher.sequential[IO](await = true)
 
@@ -53,7 +53,7 @@ class DispatcherSpec extends BaseSpec {
         TestControl.executeEmbed(action *> IO(canceled must beTrue))
       }
     }
-  }
+  }*/
 
   private def sequential(dispatcher: Resource[IO, Dispatcher[IO]]) = {
 
@@ -252,6 +252,22 @@ class DispatcherSpec extends BaseSpec {
         }
       }
     }
+
+    "respect self-cancelation" in real {
+      dispatcher use { runner =>
+        for {
+          resultR <- IO.ref(false)
+          latch <- IO.deferred[Unit]
+
+          // if the inner action is erroneously masked, `resultR` will be flipped because cancelation will be suppressed
+          _ <- IO(runner.unsafeRunAndForget((IO.canceled >> resultR.set(true)).guarantee(latch.complete(()).void)))
+          _ <- latch.get
+
+          result <- resultR.get
+          _ <- IO(result must beFalse)
+        } yield ok
+      }
+    }
   }
 
   private def awaitTermination(dispatcher: Resource[IO, Dispatcher[IO]]) = {
@@ -285,6 +301,23 @@ class DispatcherSpec extends BaseSpec {
         released2 must beTrue
       }
     }
-  }
 
+    "cancel active fibers when an error is produced" in real {
+      case object TestException extends RuntimeException
+
+      IO.deferred[Unit] flatMap { canceled =>
+        IO.deferred[Unit] flatMap { gate =>
+          val test = dispatcher use { runner =>
+            for {
+              _ <- IO(runner.unsafeRunAndForget((gate.complete(()) >> IO.never).onCancel(canceled.complete(()).void)))
+              _ <- gate.get
+              _ <- IO.raiseError(TestException).void
+            } yield ()
+          }
+
+          test.handleError(_ => ()) >> canceled.get.as(ok).timeoutTo(2.seconds, IO(false must beTrue))
+        }
+      }
+    }
+  }
 }
